2ca9a2486aae4c89e15a5b068d67ea9611092e62
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-04-19 09:11:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,3 @@ const navMenu = document.querySelector('.nav-menu');
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
-
-// JavaScript to change the CSS file on click event
-document.getElementById('changeThemeButton').addEventListener('click', function() {
-    const link = document.getElementById('themeStylesheet');
-    if (link.getAttribute('href') === 'light.css') {
-        link.setAttribute('href', 'dark.css');
-    } else {
-        link.setAttribute('href', 'light.css');
-    }
-});
